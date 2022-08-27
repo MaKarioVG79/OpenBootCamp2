97833685053906a687f2bc798665e2a7ac754b36
MaKarioVG79 <makariovg@gmail.com>
2022-08-26 21:51:58 -0500

    segundo proyecto openbootcamp

---
+++ index.js
@@ -0,0 +1,13 @@
+const fechaN = new Date(1979, 00, 09); 
+let libroPreferido = ["El Codigo Davinci", "Dan Brown", "2003", "https://es.wikipedia.org/wiki/El_c%C3%B3digo_Da_Vinci"]
+
+const nombre = {
+    nombre: "Macario VG",
+    edad: 43,
+    fecha: fechaN, 
+    desarrollador: true,
+    libro: libroPreferido,
+
+}
+
+console.log(nombre);
